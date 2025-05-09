e6aea4381a6bda0c14bfcec0e719c87410c4fcc4
Rodrigo Nemmen <rsnemmen@gmail.com>
2025-05-09 13:29:30 -0700

    added method to plot given a graph

--- hestus.py
+++ hestus.py
@@ -1,11 +1,11 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def plot_graph(segments, title=None):
+def plot(segments, title=None):
     """
-    Plots a graph provided as 
+    Plots a graph provided where segments follows the format
 
-square = (
+segments = (
     ((1, 1), (1, 2)),
     ((1, 2), (2, 2)),
     ((2, 2), (2, 1)),
@@ -37,4 +37,34 @@ square = (
     plt.gca().set_aspect("equal")
     if title:
         plt.title(title)
+    plt.show()
+
+
+def plot_graph(G):
+    """
+    G is a graph already.
+    """
+    pos = { node: node for node in G.nodes() }
+
+    plt.figure(figsize=(6,6))
+    nx.draw_networkx_nodes(
+        G, pos,
+        node_size=600,
+        node_color='cornflowerblue',
+        edgecolors='k'
+    )
+    nx.draw_networkx_edges(
+        G, pos,
+        width=3,
+        edge_color='tomato'
+    )
+
+    nx.draw_networkx_labels(
+        G, pos,
+        font_size=9
+    )
+
+    plt.gca().set_aspect('equal', 'box')  
+    plt.axis('off')
+    plt.tight_layout()
     plt.show()
